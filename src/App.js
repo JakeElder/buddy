e002f45cbe2b72a636cfa1842a5c7b0cc53e6b37
Jake Elder <jake@jpeg.me.uk>
2018-06-21 19:09:44 +0700

    Tidy up, Start creating avatar + details list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import UserInfo from './components/UserInfo';
+import Container from './components/Container';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <UserInfo />
+        <Container>
+          <UserInfo />
+        </Container>
       </div>
     );
   }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react';
 import icon from '../images/icon.svg'
 
 const UserInfo = () => (
-  <header className="Section-heading">
-   <div className="Title">
-    Account
+  <div>
+    <header className="UserInfo-heading">
+      <div className="UserInfo-title">
+        Account
+      </div>
+      <div className="UserInfo-logout">
+        Log Out
+      </div>
+    </header>
+    <div className="UserInfo-body">
+      <div className="UserInfo-avatar-and-details">
+        <div className="UserInfo-avatar">
+          <div className="Avatar">
+            <img className="Avatar-icon" src={icon} />
+            <span className="Avatar-icon-text">
+              Moderator
+            </span>
+          </div>
+        </div>
+        <div className="UserInfo-details">
+          <div className="DetailsList">
+            Code me
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="LogOut">
-    Log Out
-    </div>
-    <div className="Avatar">
-    <span className="Avatar-icon">
-      <img className="Avatar-icon-img" src={icon} />
-    </span>
-    <span className="Avatar-icon-text">
-      Moderator
-    </span>
-    </div>
-  </header>
+  </div>
 
 )
 

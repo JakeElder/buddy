ffa5afa8aced18186c54bd0500c65914fffd36e3
yan0va <anna@spiritai.com>
2018-06-25 11:28:40 +0100

    Add Alerts

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 
 const Alerts = () => (
   <div>
+    <header className="Alert-heading">
+      <div className="Alert-title">
+        Alerts
+      </div>
+    </header>
+    <div className="Alert-body">
+      <div className="Grooming-theme-color">
+        <div className="Alert-theme-and-message-count">
+          <div className="Alert-theme">
+            Grooming
+            </div>
+              <div className="Alert-message-count">
+                33 messages
+              </div>
+                <div className="Alert-time">
+                  12 minutes ago
+                </div>
+          </div>
+      </div>
+    </div>
   </div>
 )
 
